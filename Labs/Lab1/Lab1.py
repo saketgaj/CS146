a3e5cff96dc558f059d23cbb323e5a1e37439690
saketgaj <69400520+saketgaj@users.noreply.github.com>
2024-02-01 15:27:34 -0800

    Code for Lab1.py

--- Labs/Lab1/Lab1.py
+++ Labs/Lab1/Lab1.py
@@ -1,1 +1,16 @@
+def target_sum(arr, target):
+    map = {}
+    for i in range(len(arr)):
+        map[i] = target - arr[i]
+        value = map[i]
+        if value < len(arr):
+            ind = [i, value]
+            return ind
+    return None
 
+if __name__ == "__main__":
+    arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    target = 17
+    result = target_sum(arr, target)
+    if result:
+        print(result[0], result[1])
