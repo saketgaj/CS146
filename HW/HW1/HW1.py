d59b1a8e23a6c8042632a14566c62b9b61d898ad
saketgaj <69400520+saketgaj@users.noreply.github.com>
2024-02-01 15:30:44 -0800

    Code for HW1.py

--- HW/HW1/HW1.py
+++ HW/HW1/HW1.py
@@ -1,1 +1,19 @@
+def palindrome(s):
+    lower = s.lower()
+    newStr = ""
+    for i in lower:
+        if i.isdigit() or i.isalpha():
+            newStr += i
+    if len(newStr) <= 1:
+        return True
+    right = 0
+    left = len(newStr) - 1 - right
+    while right != left:
+        if newStr[right] != newStr[left]:
+            return False
+        right += 1
+        left = len(newStr) - 1 - right
+    return True
 
+if __name__ == "__main__":
+    print(palindrome("nurses run"))
