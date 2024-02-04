0bc7eabc2dd336f8d8ffbf93c17e4e97f7068ba3
saketgaj <69400520+saketgaj@users.noreply.github.com>
2024-02-03 19:19:29 -0800

    Update HW2.py

--- HW/HW2/HW2.py
+++ HW/HW2/HW2.py
@@ -1,1 +1,12 @@
+def firstBadVersion(self, n):
+    left = 1
+    right = n
 
+    while left < right:
+        mid = (left+right)/2
+        if isBadVersion(mid):
+            right = mid
+        else:
+            left = mid + 1
+    
+    return left
