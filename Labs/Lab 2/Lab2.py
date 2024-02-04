8fba94097b7d4426cc2e573037746ae890551834
saketgaj <69400520+saketgaj@users.noreply.github.com>
2024-02-03 17:40:50 -0800

    Update Lab2.py

--- Labs/Lab 2/Lab2.py
+++ Labs/Lab 2/Lab2.py
@@ -1,1 +1,13 @@
+def anagram(s, t):
+    if len(s) != len(t):
+        return False
+    sMap = {}
+    tMap = {}
+    for i in range(len(s)):
+        sMap[s[i]] = 1
+        tMap[t[i]] = 1
+    return sMap == tMap
 
+if __name__ == "__main__":
+    print(anagram("listen", "silent"))
+print(anagram("fried", "fresh"))
