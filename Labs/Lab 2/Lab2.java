fdbebf70d13fca97f901215c5e3c100cd52ab9b6
saketgaj <69400520+saketgaj@users.noreply.github.com>
2024-02-03 17:39:59 -0800

    Update Lab2.java

--- Labs/Lab 2/Lab2.java
+++ Labs/Lab 2/Lab2.java
@@ -1,1 +1,25 @@
-
+public static void main(String[] args) {
+		String s = "listen";
+		String t  = "silent";
+		String s2 = "fried";
+		String t2 = "fresh";
+		
+		System.out.println(anagram(s, t));
+		System.out.println(anagram(s2, t2));
+	}
+	
+	public static boolean anagram(String s, String t) {
+		if(s.length() != t.length()) {
+			return false;
+		}
+		
+		HashMap<Character, Integer> sMap = new HashMap<Character, Integer>();
+		HashMap<Character, Integer> tMap = new HashMap<Character, Integer>();
+		
+		for(int i = 0; i < s.length(); i++) {
+			sMap.put(s.charAt(i), 1);
+			tMap.put(t.charAt(i), 1);
+		}
+		
+		return (sMap.equals(tMap));
+	}
