a3afe25ac07da27aa02b2f5af095e520f6e0cac2
saketgaj <69400520+saketgaj@users.noreply.github.com>
2024-02-03 19:13:59 -0800

    Update HW2.java

--- HW/HW2/HW2.java
+++ HW/HW2/HW2.java
@@ -1,1 +1,14 @@
-
+public int firstBadVersion(int n) {
+  int left = 1;
+  int right = n;
+  
+  while(left < right){
+    int mid = (left+right)/2;
+    if(isBadVersion(mid)){
+    right = mid;
+    } else {
+    left = mid+1;
+    }
+  }
+  return left;
+}
