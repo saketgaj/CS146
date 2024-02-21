1665ec418f44d0f6afd68f2eaf5ea73828ca3df4
saketgaj <69400520+saketgaj@users.noreply.github.com>
2024-02-20 17:02:37 -0800

    Create HW6.py

---
+++ HW/HW6/HW6.py
@@ -0,0 +1,24 @@
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        nums.sort()
+        for i in range(len(nums)-2):
+            x = nums[i]
+            l = i+1
+            r = len(nums)-1
+            sum = 0 - x
+            while l < r:
+                if nums[l] + nums[r] == sum:
+                    triplet = [x, nums[l], nums[r]]
+                    if triplet not in result:
+                        result.append(triplet)
+                    while l < r and nums[l] == nums[l+1]:
+                        l += 1
+                    while l < r and nums[r] == nums[r-1]:
+                        r -= 1
+                    l += 1
+                    r -= 1
+                elif nums[l] + nums[r] > sum:
+                    r -= 1
+                else:
+                    l += 1
+        return result
