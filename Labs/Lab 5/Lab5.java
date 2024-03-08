f978f5dbb8fcee7fb2b12d167defa547c87a4698
saketgaj <69400520+saketgaj@users.noreply.github.com>
2024-03-08 14:49:01 -0800

    Update Lab5.java

--- Labs/Lab 5/Lab5.java
+++ Labs/Lab 5/Lab5.java
@@ -2,16 +2,15 @@ public boolean isValidBST(TreeNode root) {
         List<TreeNode> list = new LinkedList<>();
         traverse(root, list);
         
-        boolean valid = true;
         TreeNode prev = list.get(0);
         for(int i = 1; i < list.size(); i++){
             if(list.get(i).val <= prev.val){
-                valid = false;
+                return false;
             }
             prev = list.get(i);
         }
 
-        return valid;
+        return true;
 }
 
 private void traverse(TreeNode root, List<TreeNode> list) {
