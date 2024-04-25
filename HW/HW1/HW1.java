5de8154fc81eea74313a8581d46ba09f0fa68812
saketgaj <69400520+saketgaj@users.noreply.github.com>
2024-04-24 18:17:22 -0700

    Update HW1.java

--- HW/HW1/HW1.java
+++ HW/HW1/HW1.java
@@ -1,29 +1,29 @@
 public static void main(String[] args) {
-		System.out.println(palindrome("nurses run"));
-	}
-	
+	System.out.println(palindrome("nurses run"));
+}
+
 public static boolean palindrome(String s) {
-  String lower = s.toLowerCase();
-  String newStr = "";
-  for(int i = 0; i < lower.length(); i++){
-    if(Character.isLetterOrDigit(lower.charAt(i))) {
-      newStr += lower.charAt(i);
-    }
-  }
-  
-      if(newStr.length() <= 1){
-          return true;
-      }
-      
-  int right = 0;
-  int left = newStr.length()-1-right;
-  while(right != left) {
-    if(newStr.charAt(right) != newStr.charAt(left)) {
-      return false;
-    }
-     right++;
-     left = newStr.length()-1-right;
-  }
-  
-  return true;
+	String lower = s.toLowerCase();
+	String newStr = "";
+	for (int i = 0; i < lower.length(); i++) {
+		if (Character.isLetterOrDigit(lower.charAt(i))) {
+			newStr += lower.charAt(i);
+		}
 	}
+
+	if (newStr.length() <= 1) {
+		return true;
+	}
+
+	int right = 0;
+	int left = newStr.length() - 1 - right;
+	while (right != left) {
+		if (newStr.charAt(right) != newStr.charAt(left)) {
+			return false;
+		}
+		right++;
+		left = newStr.length() - 1 - right;
+	}
+
+	return true;
+}
